864ba80f0eccad21a407531a0b563561e144f04c
alonso-herreros <alonso.herreros.c@gmail.com>
2022-02-25 18:29:16 +0100

    Update APC220 test file

--- code/APC220/APC220_testing.py
+++ code/APC220/APC220_testing.py
@@ -1,11 +1,10 @@
 from serial import Serial
 from time import sleep
 
-port = Serial('/dev/ttyS0', baudrate=19200, timeout=2)
 
-port.write("Testing APC220. Reading input for 5 seconds...".encode())
-
-sleep(5)
-rcv = port.read(16)
-port.write(f"\nReceived {rcv}.\n".encode())
-print(f"Received {rcv} over serial.")
+with Serial('/dev/ttyS0', baudrate=19200, timeout=2) as radio:
+    radio.write("Testing serial comms. Reading input for 5 seconds...".encode())
+    sleep(5)
+    rcv = radio.read(16)
+    radio.write(f"\nReceived {rcv}.\n".encode())
+    print(f"Received {rcv} over serial.")
