0648152f5158291d083a77d7bb8838e6f63b70c9
alonso-herreros <alonso.herreros.c@gmail.com>
2022-04-16 18:29:22 +0200

    Update GPSReader files

--- code/Interfacing/GPSReader.py
+++ code/Interfacing/GPSReader.py
@@ -2,22 +2,28 @@ from serial import Serial
 from pathlib import Path
 from Logging.GPSLogger import GPSLogger
 import pynmea2
+import sys
+import os
 
 
 class GPSReader:
-    DEF_PORT = '/dev/ttyACM0'
-    DEF_END = 'GLL'
+    DEF_PORTS = {'win32': 'COM5', 'linux': '/dev/ttyACM0'}
+    DEF_ENDER = 'GLL'
 
 
-    def __init__(self, port=DEF_PORT, logger=True):
-        self._port = port
+    def __init__(self, port=None, log=True):
+        self._port = port or self.DEF_PORTS[sys.platform]
+        self._ser = Serial(port, 9600, timeout=1.2)
 
-        self.logger = logger
+        match log:
+            case True:
+                log_file = Path(__file__).parent.parent / 'datalogs' / 'gps_def.csv'
+            case rel if rel.startswith('.'):
+                log_file = Path(__file__).parent.parent / rel
+            case abs:
+                log_file = Path(abs)
 
-        try:
-            self._ser = Serial(self.port, 9600, timeout=0)
-        except OSError as e:
-            print(f"OSError: {e}")
+        self._logger = GPSLogger(log_file) if log else None
 
 
     def __del__(self):
@@ -25,26 +31,23 @@ class GPSReader:
 
 
     @property
-    def logger(self):
-        return self._logger
-
-    @logger.setter
-    def logger(self, val):
-        if val == True:
-            self._logger = GPSLogger(Path('.') / 'datalogs' / 'gps_def.csv')
-        else:
-            self._logger = val or None
-
-    @property
     def port(self):
         return self._port
+
+    @property
+    def has_logger(self):
+        return self._logger != None
+
+    @property
+    def log_file(self):
+        return self._logger.file_name
 
 
     def read(self, noLog=False):
         line = self._ser.readline().decode().rstrip('\r\n')
         data = pynmea2.parse(line, check=True)
 
-        if not noLog and self._logger:
+        if not noLog and self.has_logger:
             self._logger.log_sentence(data)
 
         return data
--- code/Tests/GPSReader_test.py
+++ code/Tests/GPSReader_test.py
@@ -1,5 +1,16 @@
 from Interfacing.GPSReader import GPSReader
+from pathlib import Path
+import sys
+import pytest
 
 
-def test_GPSReader():
-    pass
+PORTS = {'win32': 'COM5', 'linux': '/dev/ttyACM0'}
+BAUD = 9600
+TIMEOUT = 1.2
+
+
+def test_init():
+    gps = GPSReader()
+    assert gps.port == PORTS[sys.platform]
+    assert gps.has_logger
+    assert gps.log_file == Path(__file__).parent.parent / 'datalogs' / 'gps_def.csv'
