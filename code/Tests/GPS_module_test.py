193e818ad6a2035d1a0cce218a7c82c7046a5105
alonso-herreros <alonso.herreros.c@gmail.com>
2022-04-16 18:25:21 +0200

    Update GPS_module_test.py

--- code/Tests/GPS_module_test.py
+++ code/Tests/GPS_module_test.py
@@ -1,32 +1,69 @@
 from serial import Serial
-from time import sleep
-from matplotlib import pyplot as plt
 import pynmea2
+import sys
+import pytest
 
 
-USB_PORT = '/dev/ttyACM0'
-BAUD_RATE = 9600
+PORTS = {'win32': 'COM5', 'linux': '/dev/ttyACM0'}
+BAUD = 9600
+TIMEOUT = 1.2
+GPS_TYPES = ['TXT', 'GGA', 'GLL', 'GSA', 'GSV', 'RMC', 'VTG']
 
 
-def test_serial_port():
-    """ Test that the serial port is open """
-    with Serial(USB_PORT, BAUD_RATE) as serial_port:
-        assert serial_port.is_open
+@pytest.fixture
+def port():
+    return PORTS[sys.platform]
+
+@pytest.fixture
+def serial(port):
+    with Serial(port, BAUD, timeout=TIMEOUT) as serial:
+        yield serial
+    if serial.is_open: serial.close()
+
+@pytest.fixture
+def lines(serial):
+    return [nextline(serial) for _ in range(15)]
+
+@pytest.fixture
+def msgs(lines):
+    return [pynmea2.parse(line, check=True) for line in lines]
+
+def nextline(ser):
+    return ser.readline().decode().rstrip('\r\n')
 
 
-with Serial(USB_PORT, BAUD_RATE) as ser_gps:
-    while True:
-        line = ser_gps.readline().decode().rstrip('\r\n')
+def test_serial_port(port):
+    """ Test that the serial port can be opened """
+    with Serial(port, BAUD) as serial:
+        assert serial.is_open
 
-        msg = pynmea2.parse(line, check=True)
 
-        match msg.sentence_type:
-            case 'RMC' | 'GGA':
-                print(f"Latitude: {msg.lat:.5f}. Longitude: {msg.lon:.5f}. Altitude: {msg.alt:.5f}")
-            case 'GLL':
-                print()
+def test_receiving_data(serial):
+    """ Test that the serial port is receiving data """
+    #with Serial(PORT, BAUD, timeout=TIMEOUT) as ser:
+    assert serial.read(1)
 
-        sleep(0.1)
+
+def test_receiving_nmea_sentences(lines):
+    """ Test that the serial port is receiving NMEA sentences """
+    for line in lines:
+        assert line.startswith('$')
+        assert pynmea2.parse(line, check=True)
+
+
+def test_receiving_gps_sentences(msgs):
+    """ Test that the serial port is receiving GPS sentences """
+    for msg in msgs:
+        assert msg.sentence_type in GPS_TYPES
+
+
+def test_receiving_gps_pos(msgs):
+    """ Test that the serial port is receiving GPS position data """
+    for msg in msgs:
+        if msg.sentence_type == 'GGA':
+            assert msg.timestamp and msg.lat and msg.lon and msg.altitude
+            return
+    assert 0, 'No GGA message received'
 
 
 if __name__ == '__main__':
