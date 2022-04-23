5bd618ae574317c46cfd8d965804a92ba3ebed4d
alonso-herreros <alonso.herreros.c@gmail.com>
2022-04-23 07:18:23 +0200

    Update AtmosLogger.py

--- code/Logging/AtmosLogger.py
+++ code/Logging/AtmosLogger.py
@@ -2,7 +2,7 @@ from .DataLogger import DataLogger
 
 
 class AtmosLogger(DataLogger):
-    HEADERS = ['Time', 'Temperature', 'Pressure', 'Humidity', 'Altitude']
+    HEADERS = ['Time', 'Pressure', 'Temperature', 'Humidity', 'Altitude']
 
 
     def __init__(self, file_name, headers=None):
@@ -10,11 +10,11 @@ class AtmosLogger(DataLogger):
 
     
     def log_bmp(self, data):
-        time = data.get('time', None)
+        time = data.get('t', 0) or data.get('time', 0) or data.get('timestamp', None)
         pres = data.get('p', 0) or data.get('press', 0) or data.get('pressure', None)
         temp = data.get('T', 0) or data.get('temp', 0) or data.get('temperature', None)
-        hum = data.get('humidity', None)
-        alt = data.get('altitude', None)
+        hum  = data.get('h', 0) or data.get('hum', 0) or data.get('humidity', None)
+        alt  = data.get('a', 0) or data.get('alt', 0) or data.get('altitude', None)
         
         self.log([time, pres, temp, hum, alt])
         
