e618396c0af2a78bb296b2b3633ed46decfe8478
alonso-herreros <alonso.herreros.c@gmail.com>
2022-04-23 15:59:24 +0200

    Update AtmosLoger.py, pending implementation of log_mda

--- code/Logging/AtmosLogger.py
+++ code/Logging/AtmosLogger.py
@@ -2,19 +2,24 @@ from .DataLogger import DataLogger
 
 
 class AtmosLogger(DataLogger):
-    HEADERS = ['Time', 'Pressure', 'Temperature', 'Humidity', 'Altitude']
+    HEADERS = ['Time', 'Pressure', 'Temperature', 'Humidity']
 
 
     def __init__(self, file_name, headers=None):
         super().__init__(file_name, headers or AtmosLogger.HEADERS)
 
-    
-    def log_bmp(self, data):
-        time = data.get('t', 0) or data.get('time', 0) or data.get('timestamp', None)
-        pres = data.get('p', 0) or data.get('press', 0) or data.get('pressure', None)
-        temp = data.get('T', 0) or data.get('temp', 0) or data.get('temperature', None)
-        hum  = data.get('h', 0) or data.get('hum', 0) or data.get('humidity', None)
-        alt  = data.get('a', 0) or data.get('alt', 0) or data.get('altitude', None)
-        
-        self.log([time, pres, temp, hum, alt])
+
+    #TODO: Implement this
+    def log_mda(self, sen):
+        """ Takes a sentence and an optional timestamp and logs it."""
+        pass
+
+    #TODO: Implement alternative to log_mda by getting data as arguments somehow
+    #def log_mda(self, *args, **kwargs):
+    #    time = kwargs.get('t', 0) or data.get('time', 0) or data.get('timestamp', None)
+    #    pres = data.get('p', 0) or data.get('press', 0) or data.get('pressure', None)
+    #    temp = data.get('T', 0) or data.get('temp', 0) or data.get('temperature', None)
+    #    hum  = data.get('h', 0) or data.get('hum', 0) or data.get('humidity', None)
+    #    
+    #    self.log([time, pres, temp, hum])
         
