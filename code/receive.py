28f105ed3d9b2875eae2ebc1bdc9611b3050f26a
alonso-herreros <alonso.herreros.c@gmail.com>
2022-05-01 17:53:16 +0200

    Let's use 19200 baurdate for now

--- code/receive.py
+++ code/receive.py
@@ -2,6 +2,6 @@ from Interfacing.ArduinoReader import ArduinoReader
 
 
 if __name__ == '__main__':
-    with ArduinoReader() as arduino:
+    with ArduinoReader(baud=19200) as arduino:
         while True:
             print(arduino.readline(True))
