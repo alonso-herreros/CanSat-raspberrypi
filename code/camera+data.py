c10633e9c3dff72165d718257a0656ce998c5914
tecnoalpajes <104218593+tecnoalpajes@users.noreply.github.com>
2022-05-04 09:01:53 +0200

    Update camera+data.py

--- code/camera+data.py
+++ code/camera+data.py
@@ -15,10 +15,9 @@ except ImportError:
 bus = SMBus(1)
 bmp280 = BMP280(i2c_dev=bus)
 
-
-    
 while True: 
     for i, filename in enumerate(camera.capture_continuous("/home/cansat/timelapse/image{counter:02d}.jpg")):
+       
         temperature = bmp280.get_temperature()
         pressure = bmp280.get_pressure()
         if pressure is not None and temperature is not None:
