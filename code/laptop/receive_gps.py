ab1c1a9aecbfd7ed1af8918b578b4154e316ffff
alonso-herreros <alonso.herreros.c@gmail.com>
2022-05-07 15:28:27 +0200

    Write gps receiver script

--- code/laptop/receive_gps.py
+++ code/laptop/receive_gps.py
@@ -1,44 +1,14 @@
-from Interfacing.ArduinoReader import ArduinoReader
-from Interfacing.DataLogger import DataLogger
+from Interfacing.GPSReader import GPSReader
 from pathlib import Path
+from serial import Serial
 
 
-dir = Path() / 'logs'
-
-logs = {
-    'dump': {
-        'file': dir / 'dump.csv',
-        'fields': dict(zip(
-            ['$time', 'message'],
-            ['Time', 'Message'])),
-        'filter': lambda x: [None, str(x)] if x else None,
-    },
-    'gps': {
-        'file': dir / 'gps.csv',
-        'fields': dict(zip(
-            ['timestamp', 'lat', 'lon', 'altitude'],
-            ['Time', 'Latitude', 'Longitude', 'Altitude'])),
-        'filter': lambda x: x if getattr(x, 'sentence_type', None) == 'GGA' else None,
-    },
-    'atmos': {
-        'file': dir / 'atmos.csv',
-        'fields': dict(zip(
-            ['$time', 'b_pressure_bar', 'air_temp', 'rel_humidity'],
-            ['Time', 'Pressure (Bar)', 'Temperature (C)', 'Humidity (%)'])),
-        'filter': lambda x: x if getattr(x, 'sentence_type', None) == 'MDA' else None,
-    },
-    'txt': {
-        'file': dir / 'txt.csv',
-        'fields': dict(zip(
-            ['$time', 'text'],
-            ['Time', 'Message'])),
-        'filter': lambda x: x if getattr(x, 'sentence_type', None) == 'TXT' else None,
-    },
-}
+dir = Path() / 'logs' / 'gps'
 
 
 if __name__ == '__main__':
-    with ArduinoReader(baud=19200, loggers=logs) as arduino:
+    with GPSReader(Serial('COM7', 9600, timeout=1), dir, append=False) as receiver:
         while True:
-            line = arduino.readline(True)
+            line = receiver.readline(True)
+
             print(line)
